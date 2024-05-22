eb238ff6f47a83abaf54d767da5cffe130b1f3bb
Deep Patel <patel_deep1217@outlook.com>
2024-05-22 15:00:57 -0400

    feat: Add GlobalStyles, COLORS constants, and ThemeWrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import styled from "styled-components";
+import { COLORS } from "./constants";
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <ThemeWrapper></ThemeWrapper>;
 }
 
-export default App
+const ThemeWrapper = styled.div`
+  background-color: ${COLORS.bunker[950]};
+  color: ${COLORS.bunker[100]};
+  height: 100vh;
+  padding: 64px 32px;
+`;
+
+export default App;
